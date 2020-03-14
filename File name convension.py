dfa85fc069befe4c12c51040c95e421c93e95278
TianyiDataAnalyst <Gutian5727@gmail.com>
2020-03-14 14:41:31 -0400

    Create File name convension.py

---
+++ File name convension.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 14:38:52 2020
+
+@author: gutia
+"""
+"""
+Oanda Class name:
+Account
+Instrument
+Order
+Trade
+Position
+Transaction
+Pricing
+"""
+
+#name convension
+OandaClassName_file_name = 1
+
+#strategy file just use the strategy name as it is
